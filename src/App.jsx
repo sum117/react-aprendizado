0f2a6afc0ea3753501ba91b4d5c18219255b8d78
sum117 <jvcaliman@hotmail.com>
2022-07-05 16:29:51 -0300

    ran prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import {Post} from './components/Post';
-import { Header } from './components/Header';
-import { Sidebar } from './components/Sidebar';
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
-import './global.css';
-import styles from './App.module.css'
+import "./global.css";
+import styles from "./App.module.css";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
@@ -17,12 +17,11 @@ export function App() {
         <Sidebar />
         <main>
           <Post
-            author="Sum117" 
+            author="Sum117"
             content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo saepe minima sint corporis non voluptatem tempore magnam eligendi voluptatibus beatae harum magni cumque asperiores quibusdam enim error, dolor corrupti dolores."
           />
         </main>
       </div>
     </div>
-  )
+  );
 }
-
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,26 @@
-import styles from './Sidebar.module.css';
-import {GithubLogo} from 'phosphor-react';
+import styles from "./Sidebar.module.css";
+import { GithubLogo } from "phosphor-react";
 
 export function Sidebar() {
-    return (
-        <aside className={styles.sidebar}>
-            <img 
-                className={styles.cover} 
-                src="https://images.unsplash.com/photo-1519638399535-1b036603ac77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
-            />
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1519638399535-1b036603ac77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
 
-            <div className={styles.profile}>
-                <img className={styles.avatar} src="src\assets\sum117-bae.png"/>
-                <strong>João Caliman</strong>
-                <span>Web Developer</span>
-            </div>
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="src\assets\sum117-bae.png" />
+        <strong>João Caliman</strong>
+        <span>Web Developer</span>
+      </div>
 
-        <footer>
-            <a href='https://github.com/sum117'>
-                <GithubLogo size={20}/>
-                Acesse meu Github
-            </a>
-        </footer>
-
-        </aside>
-    );
+      <footer>
+        <a href="https://github.com/sum117">
+          <GithubLogo size={20} />
+          Acesse meu Github
+        </a>
+      </footer>
+    </aside>
+  );
 }
